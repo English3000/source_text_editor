d7158551a5463c0898fd54f1614ddf1a79b6fbd7
Alexander Katz <english3000.org@gmail.com>
2021-01-18 12:11:37 -0500

    + text display, styling ui, hideable textarea

--- lib/source_text_editor_web/live/page_live.ex
+++ lib/source_text_editor_web/live/page_live.ex
@@ -1,39 +1,26 @@
 defmodule SourceTextEditorWeb.PageLive do
   use SourceTextEditorWeb, :live_view
 
-  @impl true
+  @spec mount(any, any, Phoenix.LiveView.Socket.t()) :: {:ok, any}
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
+    {:ok, assign(socket, line_height: 2, font: "Times New Roman", content: "", edit: true)}
   end
 
-  @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
+  def handle_event("styling", state, socket) do
+    IO.inspect(state)
+    changes = []
+    changes = if line_height = Map.get(state, "line-height"), do: Keyword.put(changes, :line_height, line_height), else: changes
+    changes = if content     = Map.get(state, "content"),     do: Keyword.put(changes, :content, content),         else: changes
+    {:noreply, assign(socket, changes)}
   end
 
-  @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
+  def handle_event("font", %{"value" => font}, socket) do
+    IO.inspect(font)
+    {:noreply, assign(socket, font: font)}
   end
 
-  defp search(query) do
-    if not SourceTextEditorWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
+  def handle_event("display", %{"value" => edit}, socket) do
+    IO.inspect(edit == "true")
+    {:noreply, assign(socket, edit: if(edit == "true", do: false, else: true))}
   end
 end
